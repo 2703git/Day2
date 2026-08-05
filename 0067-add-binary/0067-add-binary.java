c2360f557537f893ca6438382ff267412d57cec1
for_mee <250182@newuu.uz>
2026-08-06 00:47:36 +0500

    Time: 1 ms (99.93%), Space: 43.6 MB (37.33%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String addBinary(String a, String b) {
+        int i = a.length()-1;
+        int j = b.length()-1;
+        int carry = 0;
+        StringBuilder ans = new StringBuilder();
+        while(i>=0 || j>=0 || carry!=0){
+            int sum = carry;
+            if(i>=0){
+            int digit = a.charAt(i) - '0';
+            i--;
+            sum += digit;
+        }
+            if(j>=0){
+            int digit = b.charAt(j) - '0';
+            j--;
+            sum += digit;
+        }
+        ans.append(sum%2);
+        carry = sum/2;
+        }
+    return ans.reverse().toString();
+    }
+}
